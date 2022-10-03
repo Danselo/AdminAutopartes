6e265e504daec5b98fbd3ff6bfd37591041a0bea
DiegoGA94 <alexgonzalez940630@gmail.com>
2022-10-02 20:25:09 -0500

    crud proveedores

--- src/components/TableProviders/TableProviders.js
+++ src/components/TableProviders/TableProviders.js
@@ -2,39 +2,51 @@ import React, { useState, useEffect } from "react";
 import "./dataTableProviders.css";
 import { Column } from "primereact/column";
 import { DataTable } from "primereact/datatable";
-import axios from  'axios';
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
 
-const url = 'http://localhost:5000/providers/'
+export const TableProviders = ({ setProviderSelected, providers }) => {
+    const [globalFilter, setGlobalFilter] = useState(null);
 
-export const TableProviders = ({idProviders}) => {
-    const [providers, setProviders] = useState([]);
-    function setProvidersId(params) {
-        
-        idProviders(params.id)       
-    }
+    const [TableProvidersSelected, setTableProvidersSelected] = useState([]);
     useEffect(() => {
-        axios.get(url).then((response) => {
-            setProviders(response.data);           
-        });
-    }, []);
+        if (TableProvidersSelected) {
+            setProviderSelected(TableProvidersSelected);
+        }
+    }, [TableProvidersSelected, setProviderSelected]);
 
     return (
-        <DataTable value={providers} 
-        paginator responsiveLayout="scroll" 
-        emptyMessage="No se encontraron datos" 
-        className="table-proveedores"
-        showGridlines
-        rows={10}  
-        onSelectionChange={e => setProvidersId(e.value) } 
-        dataKey="id" >
-            <Column field="id" sortable header="Id proveedor"></Column>
-            <Column field="nit" sortable header="Nit"></Column>
-            <Column field="nombre" sortable header="Nombre"></Column>
-            <Column field="nombre_contacto" sortable header="Nombre contacto"></Column>
-            <Column field="telefono" sortable header="Telefono"></Column>
-            <Column field="direccion" sortable header="Direccion"></Column>
-            <Column field="email" sortable header="Email"></Column>
-            <Column field="pais" sortable header="Pais"></Column>
-        </DataTable>
+        <>
+            <div className="p-inputgroup create-brand__table">
+                <InputText placeholder="Buscar proveedor" onInput={(e) => setGlobalFilter(e.target.value)} />
+                <Button icon="pi pi-search" className="p-button-primary" />
+            </div>
+
+            <DataTable
+                value={providers}
+                header="Proveedores"
+                paginator
+                responsiveLayout="scroll"
+                emptyMessage="No se encontraron datos"
+                className="table-providers"
+                showGridlines
+                stripedRows 
+                rows={10}
+                selection={TableProvidersSelected}
+                onSelectionChange={(e) => setTableProvidersSelected(e.value)}
+                dataKey="id"
+                globalFilter={globalFilter}
+            >
+                <Column selectionMode="single" headerStyle={{ width: "3em" }}></Column>
+                <Column field="id" sortable header="Id proveedor"></Column>
+                <Column field="nit" sortable header="Nit"></Column>
+                <Column field="companyName" sortable header="Nombre empresa"></Column>
+                <Column field="contactName" sortable header="Persona de contacto"></Column>
+                <Column field="telephone" sortable header="Telfono"></Column>
+                <Column field="adress" sortable header="Direccion"></Column>
+                <Column field="email" sortable header="Email"></Column>
+                <Column field="country" sortable header="Pais"></Column>
+            </DataTable>
+        </>
     );
 };
--- src/pages/Providers/Providers.js
+++ src/pages/Providers/Providers.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./providers.css";
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
@@ -6,67 +6,74 @@ import { TableProviders } from "../../components/TableProviders/TableProviders";
 import { Dialog } from "primereact/dialog";
 import { Toast } from "primereact/toast";
 import { confirmDialog } from "primereact/confirmdialog";
-import { Toolbar } from 'primereact/toolbar';
-import axios from "axios";
-import { ProvidersService } from "../../service/ProvidersService";
-const url = "http://localhost:5000/providers/create";
+import { Toolbar } from "primereact/toolbar";
+import { ProviderService } from "../../service/ProvidersService";
 
+
+const _providerService = new ProviderService();
 export default function Providers() {
+    const [providerSelected, setProviderSelected] = useState({});
     const [displayDialogCreate, setDisplayDialogCreate] = useState(false);
+    const [displayDialogEdit, setDisplayDialogEdit] = useState(false);
     const toast = useRef(null);
-    const [providersNit, setProvidersNit] = useState("");
-    const [providersCompanyName, setProvidersCompanyName] = useState("");
-    const [providersContactName, setProvidersContactName] = useState("");
-    const [providersTelephone, setProvidersTelephone] = useState("");
-    const [providersAdress, setProvidersAdress] = useState("");
-    const [providersEmail, setProvidersEmail] = useState("");
-    const [providersCountry, setProvidersCountry] = useState("");
-    const [providersId, setProvidersId] = useState("");
+    const [providerNit, setProviderNit] = useState("");
+    const [providerCompanyName, setProviderCompanyName] = useState("");
+    const [providerContactName, setProviderContactName] = useState("");
+    const [providerTelephone, setProviderTelephone] = useState("");
+    const [providerAdress, setProviderAdress] = useState("");
+    const [providerEmail, setProviderEmail] = useState("");
+    const [providerCountry, setProviderCountry] = useState("");
+    const [providers, setProviders] = useState([]);
+  
+
     const leftContents = (
         <React.Fragment>
             <Button label="Registrar" className="p-button-raised dc-space-between" icon="pi pi-plus-circle" onClick={() => onClickDialogCreate()} />
-            <Button label="Eliminar" className="p-button-raised p-button-danger dc-space-between" icon="pi pi-trash" onClick={() => deleteProvidersAlert()} />
+            <Button label="Eliminar" className="p-button-raised p-button-danger dc-space-between" icon="pi pi-trash" onClick={() => deleteProviderAlert()} disabled={!providerSelected.nit} />
+            <Button label="Editar" className="p-button-raised p-button-info dc-space-between" icon="pi pi-trash" onClick={() => onClickDialogEdit()} disabled={!providerSelected.nit} />
         </React.Fragment>
     );
 
     const rightContents = (
         <React.Fragment>
             <Button label="Desactivar" className="p-button-raised p-button-warning dc-space-between" icon="pi pi-eye-slash" onClick={() => onClickDialogCreate()} />
-        
         </React.Fragment>
     );
-    const accept = () => {
-        toast.current.show({ severity: "success", summary: "Confirmacion", detail: "Proveedor creado exitosamente", life: 3000 });
-        CreateProviders();
-    };
     const reject = () => {
         toast.current.show({ severity: "warn", summary: "Denegado", detail: "Has cancelado el proceso", life: 3000 });
     };
-    const acceptDelete = () => {
-        toast.current.show({ severity: "success", summary: "Confirmacion", detail: "Proveedor eliminado exitosamente", life: 3000 });
-        deleteProviders(providersId)
-    };
-
-    const createProviders = () => {
+    const createProviderAlert = () => {
         confirmDialog({
-            message: "¿Esta seguro que desea agregar este Proveedor?",
+            message: "¿Esta seguro que desea agregar esta provedor?",
             header: "Confirmacion",
             icon: "pi pi-exclamation-triangle",
             acceptLabel: "Crear",
             rejectLabel: "Cancelar",
-            accept,
+            accept: () => CreateProvider(),
             reject: () => setDisplayDialogCreate(true),
         });
     };
-    const deleteProvidersAlert = () => {
+
+    const editProviderAlert = () => {
         confirmDialog({
-            message: "¿Esta seguro que desea eliminar este Proveedor?",
+            message: "¿Esta seguro que desea editar esta provedor?",
+            header: "Confirmacion",
+            icon: "pi pi-exclamation-triangle",
+            acceptLabel: "Editar",
+            rejectLabel: "Cancelar",
+            accept: () => EditProvider(),
+            reject: () => setDisplayDialogEdit(false),
+        });
+    };
+    const deleteProviderAlert = () => {
+        confirmDialog({
+            message: "¿Esta seguro que desea eliminar esta Categoria?",
             header: "Confirmacion",
             icon: "pi pi-exclamation-triangle",
             acceptLabel: "Eliminar",
             rejectLabel: "Cancelar",
-            accept: () => acceptDelete(),
-            reject: () => setDisplayDialogCreate(true),
+            accept: () => deleteProvider(providerSelected),
+            reject,
         });
     };
     const cancelCreate = () => {
@@ -81,101 +88,191 @@ export default function Providers() {
             reject: () => setDisplayDialogCreate(true),
         });
     };
-    function onClickDialogCreate() {
+
+    const cancelEdit = () => {
+        confirmDialog({
+            message: "¿Esta seguro que desea perder el progreso?",
+            header: "Confirmacion",
+            icon: "pi pi-info-circle",
+            acceptClassName: "p-button-danger",
+            acceptLabel: "No editar",
+            rejectLabel: "Cancelar",
+            accept: () => reject(),
+            reject: () => setDisplayDialogEdit(true),
+        });
+    };
+    function onClickDialogCreate() {   
         setDisplayDialogCreate(true);
     }
 
+    function onClickDialogEdit() {
+        setDisplayDialogEdit(true);
+    }
+
+    const onHideDialogEdit = () => {
+        editProviderAlert();
+        setDisplayDialogEdit(false);
+    };
+
     const onHideDialogCreate = () => {
-        createProviders();
+        createProviderAlert();
         setDisplayDialogCreate(false);
     };
     const onHideDialogCreateX = () => {
         setDisplayDialogCreate(false);
     };
+    const onHideDialogEditX = () => {
+        setDisplayDialogEdit(false);
+    };
     const onHideDialogCancel = () => {
         cancelCreate();
         setDisplayDialogCreate(false);
     };
+
+    const onHideDialogCancelEdit = () => {
+        cancelEdit();
+        setDisplayDialogEdit(false);
+    };
+
+
     const renderFooterDialog = () => {
         return (
             <div>
                 <Button label="Cancelar" icon="pi pi-times" onClick={() => onHideDialogCancel()} className="p-button-text" />
-                <Button label="Crear categoria" icon="pi pi-check" onClick={() => onHideDialogCreate()} autoFocus />
+                <Button label="Crear provedor" icon="pi pi-check" onClick={() => onHideDialogCreate()} autoFocus />
             </div>
         );
     };
 
-    function CreateProviders() {
-        axios
-            .post(url, {
-                nit: providersNit,
-                companyName: providersCompanyName,
-                contactName: providersContactName,
-                telephone: providersTelephone,
-                adress: providersAdress,
-                email: providersEmail,
-                country: providersCountry,
+    const renderFooterDialogEdit = () => {
+        return (
+            <div>
+                <Button label="Cancelar" icon="pi pi-times" onClick={() => onHideDialogCancelEdit()} className="p-button-text" />
+                <Button label="Editar provedor" icon="pi pi-check" onClick={() => onHideDialogEdit()} autoFocus />
+            </div>
+        );
+    };
+    function EditProvider() {
+        _providerService
+            .updateProvider(providerSelected)
+            .then(() => {
+                setProviderSelected({});
+                loadProviders();
+                toast.current.show({ severity: "success", summary: "Confirmacion", detail: "proveedor editado exitosamente", life: 3000 });
             })
-            .then((response) => {
-                setProvidersNit(response.data);
-                setProvidersCompanyName(response.data);
-                setProvidersContactName(response.data);
-                setProvidersTelephone(response.data);
-                setProvidersAdress(response.data);
-                setProvidersEmail(response.data);
-                setProvidersCountry(response.data);
+            .catch((e) => {
+                toast.current.show({ severity: "error", summary: "Error", detail: "Upss algo salio mal al editar el proveedor", life: 3000 });
+                console.log(e);
             });
     }
-    function getProvidersIdHandler(id){
-        setProvidersId(id)
+ 
+    function CreateProvider() {
+        _providerService
+            .createProvider(providerNit, 
+                providerCompanyName,
+                providerContactName, 
+                providerTelephone, 
+                providerAdress, 
+                providerEmail,
+                providerCountry)
+            .then(() => {
+                setProviderNit("");
+                setProviderCompanyName("");
+                setProviderContactName("");
+                setProviderTelephone("");
+                setProviderEmail("");
+                setProviderAdress("");
+                setProviderCountry("");
+                loadProviders();
+                toast.current.show({ severity: "success", summary: "Confirmacion", detail: "Proveedor creado exitosamente", life: 3000 });
+            })
+            .catch((e) => {
+               toast.current.show({ severity: "error", summary: "Error", detail: "Upss algo salio mal, al crear el proveedor", life: 3000 });
+                console.log(e);
+            });
     }
 
-    const _providersService = new ProvidersService();
+    function deleteProvider(provider) {
+        _providerService
+            .deleteProvider(provider.id)
+            .then(() => {
+                toast.current.show({ severity: "success", summary: "Confirmacion", detail: "Proveedor eliminado exitosamente", life: 3000 });
+                loadProviders();
+                setProviderSelected({});
+
+            })
+            .catch((e) => {
+                toast.current.show({ severity: "error", summary: "Error", detail: "Upss algo salio mal, vuelve a intentarlo", life: 3000 });
+                console.log(e);
+            });
+    }
+
+    const loadProviders = () => {
+        _providerService.getProviders().then((response) => {
+            setProviders(response);
+        });
+    };
+
+    const onChangeProviderSelectedEditForm =  (eventOnChange)=>{
+       
+         const providerUpdated = {
+            ...providerSelected,
+            [eventOnChange.target.name]:eventOnChange.target.value
+        }
+        
+        setProviderSelected(providerUpdated)
+    }
+    useEffect(() => {
+        _providerService.getProviders().then((response) => {
+            setProviders(response);
+        });
+    }, []);
     
-    function deleteProviders(id) {
-         _providersService.deleteProviders(id)
-         .then((data) => {
-            
-             console.log('Proveedor eliminado exitosamente', data);
-         })      
-     }
     return (
         <div>
             <Toast ref={toast} />
-            <div>
-        </div>
+            <div></div>
             <div className="text-center">
                 <h4>Proveedores registrados</h4>
             </div>
 
             <Toolbar left={leftContents} right={rightContents} />
-            
-            <Dialog header="Crear nuevo proveedor" visible={displayDialogCreate} onHide={() => onHideDialogCreateX()} breakpoints={{ "960px": "75vw" }} style={{ width: "50vw" }} footer={renderFooterDialog()}>
-                <div className="create-providers-form">
-                    <h5>Ingrese el nit de la empresa</h5>
-                    <InputText value={providersNit} onChange={(e) => setProvidersNit(e.target.value)} placeholder="nit" />
-                    <h5>Ingrese el nombre de la empresa</h5>
-                    <InputText value={providersCompanyName} onChange={(e) => setProvidersCompanyName(e.target.value)} placeholder="companyName" />
-                    <h5>Ingrese el nombre del contacto</h5>
-                    <InputText value={providersContactName} onChange={(e) => setProvidersContactName(e.target.value)} placeholder="contactName" />
-                    <h5>Ingrese el Telefono de contacto</h5>
-                    <InputText value={providersTelephone} onChange={(e) => setProvidersTelephone(e.target.value)} placeholder="telephone" />
-                    <h5>Ingrese la Dirección</h5>
-                    <InputText value={providersAdress} onChange={(e) => setProvidersAdress(e.target.value)} placeholder="adress" />
-                    <h5>Ingrese el Email</h5>
-                    <InputText value={providersEmail} onChange={(e) => setProvidersEmail(e.target.value)} placeholder="email" />
-                    <h5>Ingrese el País</h5>
-                    <InputText value={providersCountry} onChange={(e) => setProvidersCountry(e.target.value)} placeholder="country" />
+
+            <Dialog header="Crear un nuevo provedor" visible={displayDialogCreate} onHide={() => onHideDialogCreateX()} breakpoints={{ "960px": "75vw" }} style={{ width: "40vw" }} footer={renderFooterDialog()}>
+                <div className="create-provider-form">
+                    <h5>Ingrese los datos del nuevo provedor</h5>
+                    <InputText value={providerNit} onChange={(e) => setProviderNit(e.target.value)} placeholder="Nit del provedor" className="create-provider-form__input" />
+                    <InputText value={providerCompanyName} onChange={(e) => setProviderCompanyName(e.target.value)} placeholder="Nombre de la empresa" className="create-provider-form__input" />
+                    <InputText value={providerContactName} onChange={(e) => setProviderContactName(e.target.value)} placeholder="Persona de contacto" className="create-provider-form__input" />
+                    <InputText value={providerTelephone} onChange={(e) => setProviderTelephone(e.target.value)} placeholder="Telefono de contacto" className="create-provider-form__input" />
+                    <InputText value={providerAdress} onChange={(e) => setProviderAdress(e.target.value)} placeholder="Direccion de la empresa" className="create-provider-form__input" />
+                    <InputText value={providerEmail} onChange={(e) => setProviderEmail(e.target.value)} placeholder="Email contacto" className="create-provider-form__input" />
+                    <InputText value={providerCountry} onChange={(e) => setProviderCountry(e.target.value)} placeholder="Pais origen proveedor" className="create-provider-form__input" />
+                   
                 </div>
-                
+            </Dialog>
+            <Dialog header="Editar provedor" visible={displayDialogEdit} onHide={() => onHideDialogEditX()} breakpoints={{ "960px": "75vw" }} style={{ width: "50vw" }} footer={renderFooterDialogEdit()}>
+                <div className="create-provider-form">
+                    <h5>Ingrese los datos del nuevo proveedor</h5>
+                    <InputText value={providerSelected.nit} onChange={onChangeProviderSelectedEditForm} 
+                    name="nit" placeholder="Nit del proveedor" className="create-provider-form__input" />
+                    <InputText value={providerSelected.companyName} onChange={onChangeProviderSelectedEditForm} 
+                    name="companyName" placeholder="Nombre de la empresal" className="create-provider-form__input" />
+                    <InputText value={providerSelected.contactName} onChange={onChangeProviderSelectedEditForm} 
+                    name="contactName" placeholder="Persona de contacto" className="create-provider-form__input" />
+                    <InputText value={providerSelected.telephone} onChange={onChangeProviderSelectedEditForm} 
+                    name="telephone" placeholder="Telefono de contacto" className="create-provider-form__input" />
+                    <InputText value={providerSelected.adress} onChange={onChangeProviderSelectedEditForm} 
+                    name="adress" placeholder="Direccion de la empresa" className="create-provider-form__input" />
+                    <InputText value={providerSelected.email} onChange={onChangeProviderSelectedEditForm} 
+                    name="email" placeholder="Email contacto" className="create-provider-form__input" />
+                    <InputText value={providerSelected.country} onChange={onChangeProviderSelectedEditForm} 
+                    name="country" placeholder="Pais origen proveedor" className="create-provider-form__input" />
+                   
+                </div>
             </Dialog>
 
-                <div className="p-inputgroup create-providers__table">
-                    <InputText placeholder="Buscar proveedor" />
-                    <Button icon="pi pi-search" className="p-button-primary" />
-                </div>
-            
-            <TableProviders className="table-products" idProviders = {(id) => getProvidersIdHandler(id)}/>
+            <TableProviders className="table-products" providers={providers} setProviderSelected={setProviderSelected} />
         </div>
     );
 }
